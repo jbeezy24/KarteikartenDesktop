c6fa71808ed7143bd95970dde9785fd221e11657
Sarah <missesgaming@gmail.com>
2020-09-14 09:02:45 +0200

    - Klasse statische Variablen + Datenbank hinzufügen

---
+++ StaticVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarteikartenDesktop
+{
+    public static class StaticVariables
+    {
+        public static string EnvironmentPath()
+        {
+            return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+        }
+    }
+}
