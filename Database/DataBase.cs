c180dd4c5c27d2367d51198eeb4de146db312927
Sarah <missesgaming@gmail.com>
2020-09-14 10:45:46 +0200

    - Datenbank: Erstellen von Tabellen

--- Database/DataBase.cs
+++ Database/DataBase.cs
@@ -26,8 +26,81 @@ namespace KarteikartenDesktop
 
                 SQLiteConnection databaseConnection = new SQLiteConnection("Data Source=karteikarten.sqlite;Version=3;");
                 databaseConnection.Open();
-                databaseConnection.Close();
+
+                SQLiteCommand mycommand = new SQLiteCommand(databaseConnection);
+                mycommand.CommandText = "PRAGMA foreign_keys=ON";
+                mycommand.ExecuteNonQuery();
+
+                this.connection = databaseConnection;
+
+                // Erstellen der Tabelle Bild mit entsprechenden Eigenschaften
+                removeTable("Bild");
+                removeTable("Intervall");
+                removeTable("Frage");
+                removeTable("Antwort");
+                removeTable("Klasse");
+                removeTable("Fach");
+                removeTable("Thema");
+                removeTable("UserSettings");
+                removeTable("Karteikarten");
+                createTable("Bild", "BildID INTEGER PRIMARY KEY AUTOINCREMENT, Bild BLOB");
+                createTable("Intervall", "IntervallID INTEGER PRIMARY KEY AUTOINCREMENT, Dauer INTEGER");
+                createTable("Frage", "FrageID INTEGER PRIMARY KEY AUTOINCREMENT, Text VARCHAR(1000), BildID INTEGER, FOREIGN KEY (BildID) REFERENCES Bild(BildID)");
+                createTable("Antwort", "AntwortID INTEGER PRIMARY KEY AUTOINCREMENT, Text VARCHAR(2000), BildID INTEGER, FOREIGN KEY (BildID) REFERENCES Bild(BildID)");
+                createTable("Klasse", "KlasseID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(64)");
+                createTable("Fach", "FachID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(64), KlasseID INTEGER, FOREIGN KEY (KlasseID) REFERENCES Klasse(KlasseID)");
+                createTable("Thema", "ThemaID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(64), FachID INTEGER, FOREIGN KEY (FachID) REFERENCES Fach(FachID)");
+                createTable("UserSettings", "SettingsID INTEGER PRIMARY KEY AUTOINCREMENT, Benutzername VARCHAR(64), Passwort VARCHAR(600), AutoLogin TINYINT, KlasseID INTEGER, FOREIGN KEY (KlasseID) REFERENCES Klasse(KlasseID)");
+                createTable("Karteikarten", "KartenID INTEGER PRIMARY KEY AUTOINCREMENT, ThemaID INTEGER, FrageID INTEGER, AntwortID INTEGER, IntervallID INTEGER, LetzteAbfrage DATETIME," +
+                    "FOREIGN KEY (ThemaID) REFERENCES Thema(ThemaID), FOREIGN KEY (FrageID) REFERENCES Frage(FrageID), FOREIGN KEY (AntwortID) REFERENCES Antwort(AntwortID), FOREIGN KEY (IntervallID) REFERENCES Intervall(IntervallID)");
+
+                this.connection.Close();
+            } else
+            {
+                SQLiteConnection databaseConnection = new SQLiteConnection("Data Source=karteikarten.sqlite;Version=3;");
+                databaseConnection.Open();
+
+                SQLiteCommand mycommand = new SQLiteCommand(databaseConnection);
+                mycommand.CommandText = "PRAGMA foreign_keys=ON";
+                mycommand.ExecuteNonQuery();
+
+                this.connection = databaseConnection;
+
+                this.connection.Close();
             }
         }
+
+        private void createTable(string tableName, string parameterString)
+        {
+            if (this.connection != null)
+            {
+                try
+                {
+                    string sql = "create table " + tableName + " (" + parameterString + ")";
+
+                    SQLiteCommand command = new SQLiteCommand(sql, this.connection);
+                    command.ExecuteNonQuery();
+                } catch (Exception ex) { 
+                    
+                }
+            }
+        }
+
+        private void removeTable(string tableName)
+        {
+            try
+            {
+                string sql = "drop table " + tableName;
+
+                SQLiteCommand command = new SQLiteCommand(sql, this.connection);
+                command.ExecuteNonQuery();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private SQLiteConnection connection;
     }
 }
