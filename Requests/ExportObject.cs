b8c7666799047b2a7c975aa760ac5c777e929415
Sarah <missesgaming@gmail.com>
2020-09-16 11:04:55 +0200

    - Import teils implementiert

---
+++ Requests/ExportObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarteikartenDesktop
+{
+    public class ExportObject
+    {
+        public Thema_Import Thema { get; set; }
+        public List<Karteikarten_Import> Karteikarten { get; set; }
+    }
+}
